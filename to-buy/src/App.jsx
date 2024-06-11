bc5a21fd8695579b9c8280b1ed23c3750aa5c7e0
larissaschottz <larissaschottz@gmail.com>
2024-06-11 17:52:36 -0300

    Salvando minhas alterações locais antes de mesclar com o remoto

--- to-buy/src/App.jsx
+++ to-buy/src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tobuy, setTobuy] = useState([
+    {
+      id: 1,
+      text: "Sabonete Facial",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Comprar sabão em pó",
+      category: "Casa",
+      isCompleted: false,
+    },
+    {
+      id: 1,
+      text: "Frutas",
+      category: "Comida",
+      isCompleted: false,
+    }
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>Olá React</div>
   )
 }
 
